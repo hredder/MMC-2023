499ae8a5153f0022436ff6305a4725643aa8d9be
Henry Redder <61098446+hredder@users.noreply.github.com>
2023-11-10 11:15:56 -0500

    pharmacy data

--- mmc.py
+++ mmc.py
@@ -11,12 +11,10 @@ alpha_pop_density =  1
 def Distance_Willing(income, kid_parant_ratio, health, insurance, vehicles, population_density):
     return alpha_income*income + alpha_kpr*kid_parant_ratio + alpha_health*health + alpha_insurance*insurance + alpha_vehicles*vehicles + alpha_pop_density*population_density
 
-count = 1000
+count = 10000
 data_x = np.random.uniform(33.75578655156453, 36.733972442257816, count)
 data_y = np.random.uniform(-84.41352382914673, -75.3388168853683, count)
 
+
 plt.scatter(data_x, data_y)
 plt.show()
-
-print(data_x)
-print(data_y)
