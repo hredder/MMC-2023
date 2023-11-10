680cea71cbf2a387e9d052cee098996e1a37017e
tobiasSimpson <93829921+tobiasSimpson@users.noreply.github.com>
2023-11-10 14:33:51 -0500

    new data

--- mmc.py
+++ mmc.py
@@ -7,16 +7,6 @@ from shapely.geometry import Polygon, Point, MultiPolygon
 import shapely
 import distance_function
 
-alpha_income =       1
-alpha_kpr =          1
-alpha_health =       1
-alpha_insurance =    1
-alpha_vehicles =     1
-alpha_pop_density =  1
-
-def Distance_Willing(income, kid_parant_ratio, health, insurance, vehicles, population_density):
-    return alpha_income*income + alpha_kpr*kid_parant_ratio + alpha_health*health + alpha_insurance*insurance + alpha_vehicles*vehicles + alpha_pop_density*population_density
-
 if __name__ == '__main__':
     count = 5000
     data_x = np.random.uniform(-84.41352382914673, -75.3388168853683, count)
